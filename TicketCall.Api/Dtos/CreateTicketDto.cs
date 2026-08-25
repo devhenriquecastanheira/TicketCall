3eb53cf27a8acafd6c9fe95b343cdedf641f5817
Henrique Cesar <henrique.fc18@gmail.com>
2026-08-25 14:21:17 -0300

    Implementando DataAnnotations nas entidades.

--- TicketCall.Api/Dtos/CreateTicketDto.cs
+++ TicketCall.Api/Dtos/CreateTicketDto.cs
@@ -1,10 +1,15 @@
-﻿using TicketCall.Api.Entities.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TicketCall.Api.Entities.Enums;
 
 namespace TicketCall.Api.Dtos;
 
 public class CreateTicketDto
 {
+    [Required]
+    [StringLength(100)]
     public string Title { get; set; } = string.Empty;
+
+    [StringLength(100)]
     public string Description { get; set; } = string.Empty;
     public Status Status { get; set; }
     public Priority Priority { get; set; }
--- TicketCall.Api/Entities/Ticket.cs
+++ TicketCall.Api/Entities/Ticket.cs
@@ -1,10 +1,17 @@
 ﻿namespace TicketCall.Api.Entities;
+
+using System.ComponentModel.DataAnnotations;
 using TicketCall.Api.Entities.Enums;
 
 public class Ticket
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
     public string Description { get; set; } = string.Empty;
     public Status Status { get; set; }
     public Priority Priority { get; set; }
