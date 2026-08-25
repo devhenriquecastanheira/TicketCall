5752869e4d0ffdc25cdc777bada24a9f7fb449f9
Henrique Cesar <henrique.fc18@gmail.com>
2026-08-25 14:15:46 -0300

    Guardando enums como string no banco de dados.

--- TicketCall.Api/Data/AppDbContext.cs
+++ TicketCall.Api/Data/AppDbContext.cs
@@ -8,4 +8,17 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Ticket>()
+            .Property(t => t.Status)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<Ticket>()
+            .Property(t => t.Priority)
+            .HasConversion<string>();
+    }
 }
